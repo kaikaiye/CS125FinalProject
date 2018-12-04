c649d7fcdbc84cd9ef33e7f90adbf59eb6089585
Owner <kaikaiy2@illinois.edu>
2018-12-03 19:40:51 -0600

    Added all java classes into Android manifest

--- app/src/main/java/com/example/owner/cs125finalproject/HomePage.java
+++ app/src/main/java/com/example/owner/cs125finalproject/HomePage.java
@@ -1,7 +1,11 @@
 package com.example.owner.cs125finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        Button start = (Button) findViewById(R.id.start_game);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Click:","Start Game");
+                Intent i = new Intent(HomePage.this, Levels_Menu.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
